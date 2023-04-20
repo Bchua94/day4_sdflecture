4b2d50152e2a30833dd51643282844a6ee32d697
Bryan Chua <Bryanchua.1994@gmail.com>
2023-04-20 12:08:53 +0800

    blackpink finished

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,13 +1,97 @@
 package sg.edu.nus.iss;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
+
+import java.util.Map;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws Exception {
+        //System.out.println( "Hello World!" );
+
+        String dirPath = args[0];
+        String fileName = args[1];
+        String dirPathFileName = dirPath + File.separator + fileName;
+        
+        File newDir = new File(dirPath);
+        if (newDir.exists()) {
+            System.out.println(dirPath + "directory already exists");
+        } else {
+            newDir.mkdir();
+        }
+
+        File newFile = new File(dirPathFileName);
+        if (!newFile.exists()) {
+            System.out.println(dirPathFileName + "file does not exists");
+            System.exit(0);
+             
+        }  
+
+        // 1. use FileReader and BufferedReader to read file
+        // a atype of decorator pattern
+        FileReader fr = new FileReader(new File(dirPathFileName));
+        BufferedReader br = new BufferedReader(fr);
+
+        StringBuilder sbfileContent = new StringBuilder();
+        String lineInput = "";
+        // 2. while loop to read file into a string variable
+        while ((lineInput = br.readLine()) !=null) {
+            sbfileContent.append(lineInput);
+
+        }
+
+        // 3. close the readers
+        br.close();
+        fr.close();
+
+        // print out the StringBuilder object sbFileContent
+        // just make sure we read all the content from the file
+        System.out.println(sbfileContent);
+
+
+        // convert the string in sbFileContent to all upper case
+        // print to screen
+        System.out.println(sbfileContent.toString().toUpperCase());
+
+        // convert StringBuilder to string
+        // so that I can use String functions to perform other tasks
+        String fileContent = sbfileContent.toString();
+        fileContent = fileContent.replace(',', ' ');
+        fileContent = fileContent.replace('.', ' ');
+        fileContent = fileContent.replace('(', ' ');
+        fileContent = fileContent.replace(')', ' ');
+        fileContent = fileContent.replace('[', ' ');
+        fileContent = fileContent.replace(']', ' ');
+        fileContent = fileContent.replace('"', ' ');
+        fileContent = fileContent.replace('?', ' ');
+        fileContent = fileContent.replace('!', ' ');
+
+        String [] fileContentArray = fileContent.split(" ");
+
+        // store all the unique words read
+        Map<String, Integer> words = new HashMap<>();
+
+        for (String word: fileContentArray) {
+
+            Integer wordExists = words.get(word);
+
+            if (wordExists == null)  {
+                // new word found
+                words.put(word,1);
+            } else {
+                
+                // the word exists in the Map.HashMap collection
+                words.put(word, wordExists + 1);
+            }
+        }
+
+        System.out.println(words);
     }
 }
